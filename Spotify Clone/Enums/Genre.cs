2f4824c0bb0049e46b25e1ffb46bea4768fc3a87
tychovr <68756557+tychovr@users.noreply.github.com>
2022-10-06 11:05:10 +0200

    Addes seperate folder for enums.

---
+++ Spotify Clone/Enums/Genre.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify_Clone
+{
+    internal class Genre
+    {
+        public enum Genres
+        {
+            Rock,
+            Pop,
+            HipHop,
+            Country,
+            Jazz,
+            Classical,
+            Blues,
+            Metal,
+            Electronic,
+            Folk,
+            Reggae,
+            RnB,
+            Soul,
+            Punk,
+            Indie,
+            Alternative,
+            World,
+            Latin,
+            Reggaeton,
+            Salsa,
+            KPop,
+            JPop,
+            Anime,
+            Soundtrack,
+            Musical,
+            Childrens,
+            Comedy,
+            SpokenWord,
+            Audiobook,
+            Podcast,
+            Radio,
+            Dance,
+            Other
+        }
+    }
+}
